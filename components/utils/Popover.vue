5cdec76605f37e6d14ecf3c0a34c5df052c7bec5
gray <jsk2343@naver.com>
2024-04-17 17:20:30 +0900

    feat: popover 컴포넌트 개발

---
+++ components/utils/Popover.vue
@@ -0,0 +1,141 @@
+<template>
+    <Teleport to="#teleports">
+        <template
+            v-if="$props.open"
+        >
+            <div class="fixed inset-0 z-[1300px]">
+                <div 
+                    :class="`opacity-0 ${isVisible ? 'animate-fade-in' : 'animate-fade-out'}`"
+                >
+                    <div
+                        class="fixed flex -z-[1] items-center justify-center bg-transparent inset-0 cursor-pointer"
+                        @click="onClose"
+                    >
+                    </div>
+                    <div
+                        class="absolute overflow-x-hidden overflow-y-auto max-w-[calc(100%-32px)] max-h-[calc(100%-32px)] outline-0 min-w-4 min-h-4 rounded-md bg-white shadow-lg"
+                        :style="[
+                            {left: anchorPos.horizontal + 'px'},
+                            {top: anchorPos.vertical + 'px'},
+                        ]"
+                    >
+                        <slot></slot>
+                    </div>
+                </div>
+            </div>
+        </template>
+    </Teleport>
+</template>
+
+<script setup lang="ts">
+
+interface PosOrigin {
+    horizontal: 'center' | 'left' | 'right' | number;
+    vertical: 'bottom' | 'center' | 'top' | number;
+}
+//type
+interface PopoverProps {
+    open?: boolean;
+    anchorEl?: HTMLElement | null;
+    anchorOrigin?: PosOrigin;
+    transformOrigin?: PosOrigin;
+}
+interface PopoverEmits {
+    (event: 'close-popover'): void;
+}
+//props
+const props = withDefaults(defineProps<PopoverProps>(), {
+    anchorOrigin: () => {
+        return {
+            horizontal: 'left',
+            vertical: 'top'
+        }
+    },
+    transformOrigin: () => {
+        return {
+            horizontal: 'left',
+            vertical: 'top'
+        }
+    },
+});
+//emit
+const emit = defineEmits<PopoverEmits>()
+//state
+const isVisible = ref(Boolean(props.open)); //popover 활성화 여부
+const anchorPos = ref({ vertical: 0, horizontal: 0 });
+const transformPos = ref({ vertical: 0, horizontal: 0 });
+
+//close event
+const onClose = () => {
+    isVisible.value = false;
+    setTimeout(() => {
+        emit('close-popover');
+    }, 170);
+}
+
+const updatePosition = () => {
+    if(props.anchorEl)
+    {
+        const el = props.anchorEl;
+        const origin = props.anchorOrigin;
+
+        const ael = el.getBoundingClientRect();
+
+        let vertical = ael.top;
+        let horizontal = ael.left;
+
+        switch (origin.vertical) {
+            case 'bottom':
+                vertical = vertical + ael.height;
+                break;
+            case 'center':
+                vertical = vertical + (ael.height/2);
+                break;
+            default:
+                vertical = ael.top;
+                break;
+        }
+
+        switch (origin.horizontal) {
+            case 'right':
+                horizontal = horizontal + ael.width;
+                break;
+            case 'center':
+                horizontal = horizontal + (ael.width/2);
+                break;
+            default:
+                horizontal = ael.left;
+                break;
+        }
+
+        anchorPos.value = {
+            vertical,
+            horizontal,
+        }
+    }
+}
+
+
+watch(() => props.open, (newPrev) => {
+    if(newPrev) {
+        updatePosition();
+        isVisible.value = true;
+        document.body.style.overflow ='hidden';
+    }
+    else
+    {
+        document.body.style.removeProperty('overflow');
+    }
+});
+
+onMounted(() => {
+    updatePosition();
+    window.addEventListener('resize', updatePosition);
+})
+
+onUnmounted(() => {
+    document.body.style.removeProperty('overflow');
+    window.removeEventListener('resize', updatePosition);
+})
+
+</script>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
         './app.vue',
         './error.vue'
     ],
+    safelist: [
+        'animate-fade-in',
+        'animate-fade-out'
+    ],
     theme: {
         extend: {
             fontFamily: {
@@ -21,9 +25,19 @@ const config: Config = {
                     '0%': { filter:'blur(0px)', width: '0px', opacity: '0.25' },
                     '100%': { filter:'blur(3px)', width: '300%', opacity: '0' },
                 },
+                'fade-in': {
+                    '0%': { opacity: '0' },
+                    '100%': { opacity: '1' },
+                },
+                'fade-out': {
+                    '0%': { opacity: '1' },
+                    '100%': { opacity: '0' },
+                },
             },
             animation: {
                 'button-ripple': 'button-ripple 0.5s linear forwards',
+                'fade-in': 'fade-in 0.15s linear forwards',
+                'fade-out': 'fade-out 0.15s linear forwards',
             }
         },
     },

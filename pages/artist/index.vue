a0a7b85c92926848af0aaf763aadf04332e001f6
gray <jsk2343@nver.com>
2024-04-23 17:29:52 +0900

    fix: 페이지 props 수정

--- pages/artist/index.vue
+++ pages/artist/index.vue
@@ -1,13 +1,22 @@
 <template>
     <Contents
-        :routes="indexRoute"
+        :routes="[
+            {
+                pathName: '메인',
+                asPath: '/'
+            },
+            {
+                pathName: '아티스트 관리'
+            }
+        ]"
         title="아티스트 관리"
         sub-title="아티스트를 생성/수정하는 페이지입니다."
         :buttons="[
             {
                 type: 'button',
                 variant: 'contained',
-                label: '생성'
+                label: '등록',
+                click: () => {$router.push('/person/form')}
             }
         ]"
     >
--- pages/person/index.vue
+++ pages/person/index.vue
@@ -1,6 +1,14 @@
 <template>
     <Contents
-        :routes="indexRoute"
+        :routes="[
+            {
+                pathName: '메인',
+                asPath: '/'
+            },
+            {
+                pathName: '인물 검색 및 등록'
+            }
+        ]"
         title="인물 검색 및 등록"
         sub-title="인물을 검색 및 등록하는 페이지입니다."
         :buttons="[
@@ -24,17 +32,6 @@
 <script setup lang="ts">
 import Contents from '@/components/layout/Contents.vue';
 import Table from '@/components/datadisplay/table/Table.vue';
-
-//컨텐츠 라우트 정보
-const indexRoute = [
-    {
-        pathName: '메인',
-        asPath: '/'
-    },
-    {
-        pathName: '인물 검색 및 등록'
-    }
-]
 
 const tableCols = [
     {
